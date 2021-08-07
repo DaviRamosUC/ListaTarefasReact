64a68348de7c9bcef041f0b923fd8bdc4ffcd665
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-08-07 12:56:57 -0300

    Apagando tarefa

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,39 +26,58 @@ export default class Main extends Component {
     this.setState({
       tarefas: [...novasTarefas, novaTarefa],
     });
+
+    document.querySelector('.entrada').value = '';
   };
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     });
+    document.querySelector('.entrada').value = e.target.value;
+  };
+
+  handleEdit = (e, index) => {
+    console.log('edit', e, index);
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
   };
 
   render() {
-    const { novaTarefa, tarefas } = this.state;
+    const { tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
         <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input
-            className="entrada"
-            onChange={this.handleChange}
-            type="text"
-            value={novaTarefa}
-          />
+          <input className="entrada" onChange={this.handleChange} type="text" />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
 
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa}>
               {tarefa}
               <span>
-                <FaEdit className="edit" />
-                <FaWindowClose className="delete" />
+                <FaEdit
+                  className="edit"
+                  onClick={(e) => this.handleEdit(e, index)}
+                />
+
+                <FaWindowClose
+                  className="delete"
+                  onClick={(e) => this.handleDelete(e, index)}
+                />
               </span>
             </li>
           ))}
