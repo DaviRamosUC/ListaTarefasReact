7a78a32135f9927cf0e522deacc254eadf7fb994
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-08-07 13:13:18 -0300

    Editando tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,33 +12,46 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
     if (tarefas.indexOf(novaTarefa) !== -1) return;
 
     const novasTarefas = [...tarefas];
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
 
-    document.querySelector('.entrada').value = '';
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
   };
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     });
-    document.querySelector('.entrada').value = e.target.value;
   };
 
   handleEdit = (e, index) => {
-    console.log('edit', e, index);
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
   };
 
   handleDelete = (e, index) => {
@@ -52,13 +65,13 @@ export default class Main extends Component {
   };
 
   render() {
-    const { tarefas } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
         <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input className="entrada" onChange={this.handleChange} type="text" />
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
           </button>
