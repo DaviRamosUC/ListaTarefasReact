a019b3369d6749b13b745df86e6e5bbe69be7146
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-08-10 13:17:30 -0300

    Eslint/adicionando as tarefas no localstorage

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
@@ -14,6 +14,9 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'no-console': 'off',
+    'react/destructuring-assignment': 'off',
+    'import/no-duplicates': 'off',
     'react/state-in-constructor': 'off',
     'react/jsx-filename-extension': 0,
     'react/prefer-stateless-function': 0,
